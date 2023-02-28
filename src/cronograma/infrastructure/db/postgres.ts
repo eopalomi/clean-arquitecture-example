47f862bb462eceb8b412c3b069995c900fee9a3d
Erick <eopalomi@gmail.com>
2023-02-28 18:11:38 -0500

    add Db config

--- src/cronograma/infrastructure/db/postgres.ts
+++ src/cronograma/infrastructure/db/postgres.ts
@@ -3,7 +3,12 @@ import { Pool } from 'pg';
 const pool = new Pool ({
     max: 20,
     //connectionString: 'postgres://root:newPassword@localhost:port/dbname',
-    connectionString: 'postgres://postgres:newPassword@postgres:5432/todo-db',
+    // connectionString: 'postgres://postgres:newPassword@postgres:5432/todo-db',
+    user:'epalomino',
+    password:'4cc3s03p4L0M1n0',
+    host: '192.168.9.250',
+    database:'prueba',
+    port: 5432,
     idleTimeoutMillis: 30000
 });
 
